ba6a6b4e7defb91fd5b04a85f0de2a52eb3cc650
Leandro Inácio <leandroinacio@gmail.com>
2022-03-30 14:38:05 -0300

    first commit

---
+++ lab-padroes-projeto-spring/src/main/java/com/digitalinnovation/gof/Aplication.java
@@ -0,0 +1,18 @@
+package com.digitalinnovation.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+@EnableFeignClients
+@SpringBootApplication
+
+public class Aplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aplication.class, args);
+
+    }
+
+}
